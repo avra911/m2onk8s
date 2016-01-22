0ec67a7be263f57f71f9c2a546eb37658bf23f74
Andrew Howden <hello@andrewhowden.com>
2016-01-22 23:05:30 +1100

    Install Magento, commit code changes to the repository
    To get the stack working, we're creating a MySQL container and using the
    command line to install things. Commands are in the Makefile!
    --
    Note: I haven't automated the creation of the MySQL container, adding
    of passwords and setting up of DB. But I did those things, and will
    probably add them to Make in future.
    
    Another Note:
    There's much I don't understand. My approach so far is "Generate all
    the things and commit them". The theory is that once generated, I can
    build the application without the DB
    
    A note on the PHP generated stuff I don't understand  yet:
    I'm a little crazy about server immutability. My primary intent with
    this Magento setup is that nothing ever changes in this container at
    all. No logs, media or other caches:
    - Logs into PHP log, which goes out via log handler
    - NGINX is in another container.
    - Media into a mounted drive
    - Other caches in Redis
    
    Magento 2 generates code. I don't know why, but I've never seen that
    pattern before (in functional classes). It's probably fine, but I
    still don't want it going on in production.
    
    My goal is to have a system that can survive periodic killing of containers
    and pods. Then, I'm regularly going to kill them (after which Kubernetes
    will replace them, and life will go on. Yay fault tolerance! I've
    never actually seen this in practice).

---
+++ package/application/app/etc/env.php
@@ -0,0 +1,62 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => '',
+  ),
+  'install' => 
+  array (
+    'date' => 'Fri, 22 Jan 2016 11:46:19 +0000',
+  ),
+  'crypt' => 
+  array (
+    'key' => '',
+  ),
+  'session' => 
+  array (
+    'save' => 'files',
+  ),
+  'db' => 
+  array (
+    'table_prefix' => '',
+    'connection' => 
+    array (
+      'default' => 
+      array (
+        'host' => '127.0.0.1',
+        'dbname' => 'magento2',
+        'username' => '',
+        'password' => '',
+        'model' => 'mysql4',
+        'engine' => 'innodb',
+        'initStatements' => 'SET NAMES utf8;',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'default',
+  'cache_types' => 
+  array (
+    'config' => 1,
+    'layout' => 1,
+    'block_html' => 1,
+    'collections' => 1,
+    'reflection' => 1,
+    'db_ddl' => 1,
+    'eav' => 1,
+    'config_integration' => 1,
+    'config_integration_api' => 1,
+    'full_page' => 1,
+    'translate' => 1,
+    'config_webservice' => 1,
+    'compiled_config' => 1,
+  ),
+);
