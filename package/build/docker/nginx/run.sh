a7252076491207c73db7cd59c0f4d37b2f57226f
Andrew Howden <hello@andrewhowden.com>
2016-02-28 17:41:36 +1100

    Rebuild lb container based on alpine:edge + nginx, remove DNS cache

--- package/build/docker/nginx/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Overwrite the DNS resolver to use the one in the local address space
-echo "nameserver 127.0.0.1" > /etc/resolv.conf
-
-cat /etc/resolv.conf > /proc/self/fd/1
-
-# [CMD]
-nginx -g "daemon off;"
