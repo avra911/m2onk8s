ba40d039fab7395a27465fe7ec7e3d7dd853a02f
Andrew Howden <hello@andrewhowden.com>
2016-02-28 16:40:37 +1100

    Replace resolver in lb stack, output resolv.conf to std out on nginx boot

--- package/build/docker/nginx/run.sh
+++ package/build/docker/nginx/run.sh
@@ -3,5 +3,7 @@
 # Overwrite the DNS resolver to use the one in the local address space
 echo "namesever 127.0.0.1" > /etc/resolve.conf
 
+cat /etc/resolv.conf > /proc/self/fd/1
+
 # [CMD]
-nginx
+nginx -g "deamon off"
