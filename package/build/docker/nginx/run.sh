1b47768fee2f817c0e571c9b49b321bc7fde82e5
Andrew Howden <hello@andrewhowden.com>
2016-02-28 16:47:42 +1100

    Address some syntax errors in nginx run.sh

--- package/build/docker/nginx/run.sh
+++ package/build/docker/nginx/run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # Overwrite the DNS resolver to use the one in the local address space
-echo "namesever 127.0.0.1" > /etc/resolve.conf
+echo "nameserver 127.0.0.1" > /etc/resolv.conf
 
 cat /etc/resolv.conf > /proc/self/fd/1
 
 # [CMD]
-nginx -g "deamon off"
+nginx -g "deamon off;"
